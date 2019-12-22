2b1a67b3c8b52838b92da10f88861de11a63e31b
Tom <tomdymel@web.de>
2019-12-22 13:06:03 +0000

    Temporary different domain

--- Deploy/init.sh
+++ Deploy/init.sh
@@ -1,6 +1,6 @@
 REPOSITORY_NAME='LegacyPlayersV3'
 REPOSITORY='https://github.com/Geigerkind/LegacyPlayersV3'
-DOMAIN='beta.legacyplayers.com'
+DOMAIN='yajla.com'
 HOST_USER='root'
 BACKEND_USER='rpll'
 HOST_IP='51.38.99.189'
