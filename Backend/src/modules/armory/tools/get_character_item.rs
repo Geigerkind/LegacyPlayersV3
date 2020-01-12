738ec5d9e845c4dd86c285c2bee520aa6953c4a6
Tom <tomdymel@web.de>
2020-01-12 12:19:05 +0000

    Implementing get character item by id

--- Backend/src/modules/armory/tools/get_character_item.rs
+++ Backend/src/modules/armory/tools/get_character_item.rs
@@ -1,6 +1,7 @@
 use crate::dto::Failure;
 use crate::modules::armory::Armory;
 use crate::modules::armory::domain_value::CharacterItem;
+use mysql_connection::tools::Select;
 
 pub trait GetCharacterItem {
   fn get_character_item(&self, character_item_id: u32) -> Result<CharacterItem, Failure>;
@@ -8,6 +9,23 @@ pub trait GetCharacterItem {
 
 impl GetCharacterItem for Armory {
   fn get_character_item(&self, character_item_id: u32) -> Result<CharacterItem, Failure> {
-    unimplemented!()
+    let params = params!(
+      "id" => character_item_id
+    );
+    self.db_main.select_wparams_value("SELECT * FROM armory_item WHERE id=:id", &|mut row| {
+      CharacterItem {
+        id: row.take(0).unwrap(),
+        item_id: row.take(1).unwrap(),
+        random_property_id: row.take_opt(2).unwrap().ok(),
+        enchant_id: row.take_opt(3).unwrap().ok(),
+        gem_ids: vec![
+          row.take_opt(4).unwrap().ok(),
+          row.take_opt(5).unwrap().ok(),
+          row.take_opt(6).unwrap().ok(),
+          row.take_opt(7).unwrap().ok()
+        ]
+      }
+    }, params);
+    Err(Failure::Unknown)
   }
 }
