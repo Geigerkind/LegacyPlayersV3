89d3323d2a9118d90564f4463101440946996442
Tom <tomdymel@web.de>
2020-01-12 17:54:05 +0000

    Implementing delete character utility

--- Backend/src/modules/armory/tools/delete_character.rs
+++ Backend/src/modules/armory/tools/delete_character.rs
@@ -1,5 +1,6 @@
 use crate::dto::Failure;
 use crate::modules::armory::Armory;
+use mysql_connection::tools::Execute;
 
 pub trait DeleteCharacter {
   fn delete_character(&self, id: u32) -> Result<(), Failure>;
@@ -7,6 +8,12 @@ pub trait DeleteCharacter {
 
 impl DeleteCharacter for Armory {
   fn delete_character(&self, id: u32) -> Result<(), Failure> {
-    unimplemented!()
+    let mut characters = self.characters.write().unwrap();
+    if self.db_main.execute_wparams("DELETE FROM armory_character WHERE id=:id", params!(
+      "id" => id
+    )) {
+      return characters.remove(&id).ok_or(Failure::InvalidInput).and_then(|_| Ok(()));
+    }
+    Err(Failure::Unknown)
   }
 }
