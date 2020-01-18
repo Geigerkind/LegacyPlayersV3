d9da31fde38b0cfc3fb9f32adeb56b63388f67e8
Tom Dymel <td@wps.de>
2020-01-18 10:49:31 +0100

    update guild test and fixes

--- Backend/src/modules/armory/tests/guild.rs
+++ Backend/src/modules/armory/tests/guild.rs
@@ -1,6 +1,6 @@
 use crate::modules::armory::dto::GuildDto;
 use crate::modules::armory::Armory;
-use crate::modules::armory::tools::{CreateGuild, GetGuild};
+use crate::modules::armory::tools::{CreateGuild, GetGuild, UpdateGuild};
 use mysql_connection::tools::Execute;
 
 #[test]
@@ -22,6 +22,19 @@ fn guild() {
 
   let guild2 = guild_res2.unwrap();
   assert!(guild2.deep_eq(&guild));
+
+  // update the guild
+  let new_guild_name = "Test123".to_string();
+  let update_res = armory.update_guild_name(1, guild_dto.server_uid, new_guild_name.clone());
+  assert!(update_res.is_ok());
+
+  let guild_res3 = armory.get_guild(guild.id);
+  assert!(guild_res3.is_some());
+
+  let guild3 = guild_res3.unwrap();
+  assert_eq!(guild3.server_uid, guild_dto.server_uid);
+  assert_ne!(guild3.name, guild_dto.name);
+  assert_eq!(guild3.name, new_guild_name);
 
   armory.db_main.execute_wparams("DELETE FROM armory_guild WHERE id=:id", params!("id" => guild.id));
 }
--- Backend/src/modules/armory/tools/update_guild.rs
+++ Backend/src/modules/armory/tools/update_guild.rs
@@ -10,13 +10,13 @@ pub trait UpdateGuild {
 
 impl UpdateGuild for Armory {
   fn update_guild_name(&self, server_id: u32, uid: u64, guild_name: String) -> Result<(), Failure> {
+    let guild_id = self.get_guild_id_by_uid(server_id, uid).unwrap();
     let mut guilds = self.guilds.write().unwrap();
     if self.db_main.execute_wparams("UPDATE armory_guild SET guild_name=:guild_name WHERE server_id=:server_id AND server_uid=:server_uid", params!(
       "server_id" => server_id,
       "server_uid" => uid,
       "guild_name" => guild_name.clone()
     )) {
-      let guild_id = self.get_guild_id_by_uid(server_id, uid).unwrap();
       let mut guild = guilds.get_mut(&guild_id).unwrap();
       guild.name = guild_name.to_owned();
       return Ok(());
