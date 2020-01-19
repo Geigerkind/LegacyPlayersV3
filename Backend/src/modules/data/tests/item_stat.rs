d576a85558172802653d68cc3b408e2801f4fd75
Tom <tomdymel@web.de>
2020-01-19 20:32:40 +0000

    Fix item stat test

--- Backend/src/modules/data/tests/item_stat.rs
+++ Backend/src/modules/data/tests/item_stat.rs
@@ -4,12 +4,12 @@ use crate::modules::data::tools::RetrieveItemStat;
 #[test]
 fn get_item_stats() {
   let data = Data::default().init(Some(28));
-  let item_stats = data.get_item_stats(1, 39);
+  let item_stats = data.get_item_stats(1, 940);
   assert!(item_stats.is_some());
   let item_stats_vec = item_stats.unwrap();
   assert!(item_stats_vec.len() > 0);
   assert_eq!(item_stats_vec[0].expansion_id, 1);
-  assert_eq!(item_stats_vec[0].item_id, 39);
+  assert_eq!(item_stats_vec[0].item_id, 940);
   let no_item_stats = data.get_item_stats(0, 0);
   assert!(no_item_stats.is_none());
 }
