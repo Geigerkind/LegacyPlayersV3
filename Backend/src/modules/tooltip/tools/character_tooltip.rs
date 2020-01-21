3399781b443ca68aa93379066703590689fa73b3
Tom Dymel <td@wps.de>
2020-01-21 15:26:17 +0100

    Implementing character tooltip

--- Backend/src/modules/tooltip/tools/character_tooltip.rs
+++ Backend/src/modules/tooltip/tools/character_tooltip.rs
@@ -2,13 +2,41 @@ use crate::modules::tooltip::material::CharacterTooltip;
 use crate::dto::Failure;
 use crate::modules::tooltip::Tooltip;
 use crate::modules::armory::Armory;
+use crate::modules::armory::tools::{GetCharacter, GetGuild};
+use crate::modules::tooltip::domain_value::CharacterGuild;
+use crate::modules::data::Data;
+use crate::modules::data::tools::RetrieveServer;
 
 pub trait RetrieveCharacterTooltip {
-    fn get_character(&self, armory: &Armory, character_id: u32) -> Result<CharacterTooltip, Failure>;
+    fn get_character(&self, data: &Data, armory: &Armory, character_id: u32) -> Result<CharacterTooltip, Failure>;
 }
 
 impl RetrieveCharacterTooltip for Tooltip {
-    fn get_character(&self, armory: &Armory, character_id: u32) -> Result<CharacterTooltip, Failure> {
-        unimplemented!()
+    fn get_character(&self, data: &Data, armory: &Armory, character_id: u32) -> Result<CharacterTooltip, Failure> {
+        let character_res = armory.get_character(character_id);
+        if character_res.is_none() {
+            return Err(Failure::InvalidInput);
+        }
+        let character = character_res.unwrap();
+
+        if character.last_update.is_none() {
+            return Err(Failure::Unknown); // TODO: Better failure here
+        }
+        let character_history = character.last_update.unwrap();
+
+        // TODO: Improve data representation here
+        let guild_rank = character_history.guild_rank.to_owned();
+        let guild = character_history.guild_id
+            .and_then(|id| armory.get_guild(id)
+                .and_then(|guild| Some(CharacterGuild {
+                    name: guild.name.to_owned(),
+                    rank: guild_rank.unwrap().to_owned()
+                })));
+
+        Ok(CharacterTooltip {
+            name: character_history.character_name.to_owned(),
+            server: data.get_server(character.server_id).unwrap().name.to_owned(),
+            guild
+        })
     }
 }
--- Backend/src/modules/tooltip/transfer/character_tooltip.rs
+++ Backend/src/modules/tooltip/transfer/character_tooltip.rs
@@ -3,13 +3,14 @@ use rocket_contrib::json::Json;
 
 use crate::dto::Failure;
 use crate::modules::armory::Armory;
+use crate::modules::data::Data;
 use crate::modules::tooltip::material::CharacterTooltip;
 use crate::modules::tooltip::tools::RetrieveCharacterTooltip;
 use crate::modules::tooltip::Tooltip;
 
 #[openapi]
 #[get("/character/<id>")]
-pub fn get_character(me: State<Tooltip>, armory: State<Armory>, id: u32) -> Result<Json<CharacterTooltip>, Failure>
+pub fn get_character(me: State<Tooltip>, data: State<Data>, armory: State<Armory>, id: u32) -> Result<Json<CharacterTooltip>, Failure>
 {
-    me.get_character(&armory, id).and_then(|tooltip| Ok(Json(tooltip)))
+    me.get_character(&data, &armory, id).and_then(|tooltip| Ok(Json(tooltip)))
 }
