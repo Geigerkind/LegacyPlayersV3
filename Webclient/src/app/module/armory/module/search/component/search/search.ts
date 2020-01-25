ddb7ddc94fd502e70a9c84159f2b1685d3696dae
Tom <tomdymel@web.de>
2020-01-25 21:26:24 +0000

    Implementing client side guild

--- Webclient/src/app/module/armory/module/search/component/search/search.ts
+++ Webclient/src/app/module/armory/module/search/component/search/search.ts
@@ -14,12 +14,13 @@ export class SearchComponent {
 
     character_header_columns: Array<HeaderColumn> = [
         { index: 0, filter_name: 'name', labelKey: "Name", type: 0, type_range: null },
-        { index: 1, filter_name: 'gender', labelKey: "Gender", type: 3, type_range: ['General.gender_0', 'General.gender_1'] },
-        { index: 2, filter_name: 'server', labelKey: "Server", type: 3, type_range: ['0', '1', '2'] },
-        { index: 3, filter_name: 'race', labelKey: "Race", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
-        { index: 4, filter_name: 'faction', labelKey: "Faction", type: 3, type_range: ['General.faction_0', 'General.faction_1'] },
-        { index: 5, filter_name: 'hero_class', labelKey: "Class", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
-        { index: 6, filter_name: 'last_updated', labelKey: "Last update", type: 2, type_range: null },
+        { index: 1, filter_name: 'guild', labelKey: "Guild", type: 0, type_range: null },
+        { index: 2, filter_name: 'gender', labelKey: "Gender", type: 3, type_range: ['General.gender_0', 'General.gender_1'] },
+        { index: 3, filter_name: 'server', labelKey: "Server", type: 3, type_range: ['0', '1', '2'] },
+        { index: 4, filter_name: 'race', labelKey: "Race", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
+        { index: 5, filter_name: 'faction', labelKey: "Faction", type: 3, type_range: ['General.faction_0', 'General.faction_1'] },
+        { index: 6, filter_name: 'hero_class', labelKey: "Class", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
+        { index: 7, filter_name: 'last_updated', labelKey: "Last update", type: 2, type_range: null },
     ];
 
     character_body_columns: Array<Array<BodyColumn>> = [];
