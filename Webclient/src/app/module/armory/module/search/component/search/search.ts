a99222970f115056c2d763919387a31a2771c31b
Tom <tomdymel@web.de>
2020-01-25 22:49:28 +0000

    Armory search header titles

--- Webclient/src/app/module/armory/module/search/component/search/search.ts
+++ Webclient/src/app/module/armory/module/search/component/search/search.ts
@@ -13,14 +13,14 @@ import {init_body_columns_from_result} from "../../../../../../template/table/ut
 export class SearchComponent {
 
     character_header_columns: Array<HeaderColumn> = [
-        { index: 0, filter_name: 'name', labelKey: "Name", type: 0, type_range: null },
-        { index: 1, filter_name: 'guild', labelKey: "Guild", type: 0, type_range: null },
-        { index: 2, filter_name: 'gender', labelKey: "Gender", type: 3, type_range: ['General.gender_0', 'General.gender_1'] },
-        { index: 3, filter_name: 'server', labelKey: "Server", type: 3, type_range: ['0', '1', '2'] },
-        { index: 4, filter_name: 'race', labelKey: "Race", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
-        { index: 5, filter_name: 'faction', labelKey: "Faction", type: 3, type_range: ['General.faction_0', 'General.faction_1'] },
-        { index: 6, filter_name: 'hero_class', labelKey: "Class", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
-        { index: 7, filter_name: 'last_updated', labelKey: "Last update", type: 2, type_range: null },
+        { index: 0, filter_name: 'name', labelKey: "Armory.Search.name", type: 0, type_range: null },
+        { index: 1, filter_name: 'guild', labelKey: "Armory.Search.guild", type: 0, type_range: null },
+        { index: 2, filter_name: 'gender', labelKey: "Armory.Search.gender", type: 3, type_range: ['General.gender_0', 'General.gender_1'] },
+        { index: 3, filter_name: 'server', labelKey: "Armory.Search.server", type: 3, type_range: ['0', '1', '2'] },
+        { index: 4, filter_name: 'race', labelKey: "Armory.Search.race", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
+        { index: 5, filter_name: 'faction', labelKey: "Armory.Search.faction", type: 3, type_range: ['General.faction_0', 'General.faction_1'] },
+        { index: 6, filter_name: 'hero_class', labelKey: "Armory.Search.hero_class", type: 3, type_range: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] },
+        { index: 7, filter_name: 'last_updated', labelKey: "Armory.Search.last_update", type: 2, type_range: null },
     ];
 
     character_body_columns: Array<Array<BodyColumn>> = [];
