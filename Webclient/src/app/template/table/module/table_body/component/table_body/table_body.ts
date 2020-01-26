33fe96fbd43c734fcb9c7a4509a4f86f19ddab3e
Tom <tomdymel@web.de>
2020-01-26 12:39:44 +0000

    Introducing hack to busy load the type_range definition

--- Webclient/src/app/template/table/module/table_body/component/table_body/table_body.ts
+++ Webclient/src/app/template/table/module/table_body/component/table_body/table_body.ts
@@ -17,11 +17,21 @@ export class TableBodyComponent implements OnChanges {
     internalTypeRange: Map<number,string>[] = [];
 
     ngOnChanges(): void {
+        this.updateInternalTypeRange();
+    }
+
+    updateInternalTypeRange(): void {
         this.headColumns.forEach(item => {
             if (this.internalTypeRange.length <= item.index)
                 this.internalTypeRange.push(new Map<number, string>());
-            if (item.type_range !== null)
+            if (item.type === 3) {
                 item.type_range.forEach(entry => this.internalTypeRange[item.index].set(entry.value, entry.labelKey));
+
+                // Horrible hack, but it circumvents Angular's problem with property bindings on objects
+                if (this.internalTypeRange[item.index].size === 1) {
+                    setTimeout(() => this.updateInternalTypeRange(), 50);
+                }
+            }
         });
     }
 }
