aae165910244c961afed51b201d36f34f4647306
Tom <tomdymel@web.de>
2020-02-07 19:24:50 +0000

    A sample endpoint to test the forwarding of the account token

---
+++ Exporter/Sample_endpoint/src/main.rs
@@ -0,0 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+
+use rocket_contrib::json::Json;
+
+#[get("/<token>/<account_id>")]
+fn validate_token(token: String, account_id: u32) -> Json<bool> {
+  Json(token == "abc" && account_id == 42)
+}
+
+fn main() {
+  rocket::ignite()
+    .mount("/token_validator/", routes![
+      validate_token
+    ])
+    .launch();
+}
