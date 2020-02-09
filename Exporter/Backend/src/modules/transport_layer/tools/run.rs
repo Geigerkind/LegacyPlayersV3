07b47b9143c0bd2e3535bea58fe5323efdc8bddd
Tom <tomdymel@web.de>
2020-02-09 12:49:05 +0000

    Removing debug api token and specifying installation process a little better

--- Exporter/Backend/src/modules/transport_layer/tools/run.rs
+++ Exporter/Backend/src/modules/transport_layer/tools/run.rs
@@ -9,7 +9,7 @@ use reqwest::header::{HeaderValue, CONTENT_TYPE};
 impl Run for TransportLayer {
   fn run(&mut self) {
     let rate = env::var("REQUESTS_TO_LP_PER_SECOND").unwrap().parse::<f64>().unwrap();
-    let api_token = env::var("API_TOKEN").unwrap();
+    let api_token = env::var("LP_API_TOKEN").unwrap();
     let url_set_character = env::var("URL_SET_CHARACTER").unwrap();
 
     let sleep_duration_rate = Duration::new(0, (1000000000.0 as f64 * (1.0 / rate)).ceil() as u32);
