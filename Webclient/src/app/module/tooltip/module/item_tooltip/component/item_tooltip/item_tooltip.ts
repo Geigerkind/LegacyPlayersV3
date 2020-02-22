f928ab6ae381da027feead6553f763b9746a1006
Tom Dymel <td@wps.de>
2020-02-22 11:31:34 +0100

    Item tooltip set items display

--- Webclient/src/app/module/tooltip/module/item_tooltip/component/item_tooltip/item_tooltip.ts
+++ Webclient/src/app/module/tooltip/module/item_tooltip/component/item_tooltip/item_tooltip.ts
@@ -24,11 +24,21 @@ export class ItemTooltipComponent {
 
     getFilteredSetItems(): any {
         return this.payload.item_set.set_items
-            .filter(item => item.active || item.item_level === this.payload.item_level)
             .sort((left, right) => {
-                if (left.active === right.active) return 0;
-                if (left.active !== right.active) return 1;
-                return -1;
+                if (left.active === right.active) {
+                    if (left.inventory_type === right.inventory_type) {
+                        if (left.item_level === right.inventory_type) {
+                            return 0;
+                        } else if (left.item_level < right.item_level) {
+                            return 1;
+                        }
+                        return -1;
+                    } else if (left.inventory_type > right.inventory_type) {
+                        return 1;
+                    }
+                    return -1;
+                }
+                return 1;
             })
             .reduce((acc, item) => {
                 if (item.active || !acc.find(inner_item => inner_item.inventory_type === item.inventory_type))
