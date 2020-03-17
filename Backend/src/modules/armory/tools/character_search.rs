0b15367b89ce12185df73b96319204975690f659
Tom Dymel <td@wps.de>
2020-03-17 22:53:35 +0100

    An interestingly not significant faster way of filtering

--- Backend/src/modules/armory/tools/character_search.rs
+++ Backend/src/modules/armory/tools/character_search.rs
@@ -13,6 +13,7 @@ use crate::{
     },
 };
 use crate::util::ordering::NegateOrdExt;
+use crate::modules::armory::material::{Character, Guild};
 
 pub trait PerformCharacterSearch {
     fn get_character_search_result(&self, data: &Data, filter: CharacterSearchFilter) -> SearchResult<CharacterSearchResult>;
@@ -29,10 +30,8 @@ impl PerformCharacterSearch for Armory {
         if filter.guild.filter.is_some() {
             *filter.guild.filter.as_mut().unwrap() = filter.guild.filter.as_ref().unwrap().to_lowercase();
         }
-        let mut result: Vec<CharacterSearchResult> = characters
-            .iter()
-            .filter(|(_, character)| character.last_update.is_some())
-            .filter(|(_, character)| filter.server.filter.is_none() || filter.server.filter.contains(&character.server_id))
+        let result = characters.iter().filter(|(_, character)| character.last_update.is_some());
+        let mut result:Vec<(&Character, Option<Guild>)> = result.filter(|(_, character)| filter.server.filter.is_none() || filter.server.filter.contains(&character.server_id))
             .filter(|(_, character)| filter.name.filter.is_none() || character.last_update.as_ref().unwrap().character_name.to_lowercase().contains(filter.name.filter.as_ref().unwrap()))
             .filter(|(_, character)| filter.hero_class.filter.is_none() || filter.hero_class.filter.contains(&character.last_update.as_ref().unwrap().character_info.hero_class_id))
             .filter(|(_, character)| {
@@ -43,47 +42,47 @@ impl PerformCharacterSearch for Armory {
                 let current_timestamp = character.last_update.as_ref().unwrap().timestamp;
                 current_timestamp >= filter_timestamp && current_timestamp <= filter_timestamp + 24 * 60 * 60
             })
-            // This is very expensive
-            .filter(|(_, character)| {
+            .filter_map(|(_, character)| {
                 if filter.guild.filter.is_none() {
-                    return true;
+                    return Some((character, None));
                 }
                 if let Some(character_guild) = character.last_update.as_ref().unwrap().character_guild.as_ref() {
                     if let Some(guild) = self.get_guild(character_guild.guild_id) {
-                        return guild.name.to_lowercase().contains(filter.guild.filter.as_ref().unwrap());
+                        if guild.name.to_lowercase().contains(filter.guild.filter.as_ref().unwrap()) {
+                            return Some((character, Some(guild)));
+                        }
                     }
                 }
-                false
-            })
-            .map(|(_, character)| {
-                let race_id = character.last_update.as_ref().unwrap().character_info.race_id;
-                CharacterSearchResult {
-                    faction: data.get_race(race_id).unwrap().faction,
-                    guild: character
-                        .last_update.as_ref().unwrap().character_guild.as_ref()
-                        .and_then(|character_guild| self.get_guild(character_guild.guild_id).map(|guild| CharacterSearchGuildDto { guild_id: guild.id, name: guild.name })),
-                    character: CharacterSearchCharacterDto {
-                        character_id: character.id,
-                        name: character.last_update.as_ref().unwrap().character_name.clone(),
-                        hero_class_id: character.last_update.as_ref().unwrap().character_info.hero_class_id,
-                        server_id: character.server_id,
-                    },
-                    timestamp: character.last_update.as_ref().unwrap().timestamp,
-                }
+                None
             })
             .collect();
         let num_characters = result.len();
 
-        result.sort_by(|left, right| rpll_table_sort! {
-            (filter.hero_class, Some(&left.character.hero_class_id), Some(&right.character.hero_class_id)),
-            (filter.name, Some(&left.character.name), Some(&right.character.name)),
-            (filter.guild, left.guild.as_ref().map(|gld| &gld.name), right.guild.as_ref().map(|gld| &gld.name)),
-            (filter.server, Some(&left.character.server_id), Some(&right.character.server_id)),
-            (filter.last_updated, Some(&left.timestamp), Some(&right.timestamp))
+        result.sort_by(|(l_char, l_guild), (r_char, r_guild)| rpll_table_sort! {
+            (filter.hero_class, Some(&l_char.last_update.as_ref().unwrap().character_info.hero_class_id), Some(&r_char.last_update.as_ref().unwrap().character_info.hero_class_id)),
+            (filter.name, Some(&l_char.last_update.as_ref().unwrap().character_name), Some(&r_char.last_update.as_ref().unwrap().character_name)),
+            (filter.guild, l_guild.as_ref().map(|gld| &gld.name), r_guild.as_ref().map(|gld| &gld.name)),
+            (filter.server, Some(&l_char.server_id), Some(&r_char.server_id)),
+            (filter.last_updated, Some(&l_char.last_update.as_ref().unwrap().timestamp), Some(&r_char.last_update.as_ref().unwrap().timestamp))
         });
 
         SearchResult {
-            result: result.iter().skip((filter.page * 10) as usize).take(10).map(|cs| cs.to_owned()).collect::<Vec<CharacterSearchResult>>(),
+            result: result.iter().skip((filter.page * 10) as usize).take(10)
+                .map(|(character, guild)| {
+                    let race_id = character.last_update.as_ref().unwrap().character_info.race_id;
+                    CharacterSearchResult {
+                        faction: data.get_race(race_id).unwrap().faction,
+                        guild: guild.as_ref().map(|innr_gld| CharacterSearchGuildDto { guild_id: innr_gld.id, name: innr_gld.name.clone() }),
+                        character: CharacterSearchCharacterDto {
+                            character_id: character.id,
+                            name: character.last_update.as_ref().unwrap().character_name.clone(),
+                            hero_class_id: character.last_update.as_ref().unwrap().character_info.hero_class_id,
+                            server_id: character.server_id,
+                        },
+                        timestamp: character.last_update.as_ref().unwrap().timestamp,
+                    }
+                })
+                .collect::<Vec<CharacterSearchResult>>(),
             num_items: num_characters,
         }
     }
