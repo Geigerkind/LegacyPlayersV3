6e1342ff0c475d27fe757bba72e335fbe4d7a3b2
Tom Dymel <td@wps.de>
2020-04-03 10:59:45 +0200

    Mapping SpellCast

--- Backend/src/modules/live_data_processor/tools/payload_mapper/spell_cast.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/spell_cast.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, SpellCast};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapSpellCast {
   fn to_spell_cast(&self) -> Result<SpellCast, LiveDataProcessorFailure>;
@@ -6,6 +7,12 @@ pub trait MapSpellCast {
 
 impl MapSpellCast for [u8] {
   fn to_spell_cast(&self) -> Result<SpellCast, LiveDataProcessorFailure> {
-    unimplemented!()
+    let target_id = byte_reader::read_u64(&self[8..16])?;
+    Ok(SpellCast {
+      caster: byte_reader::read_u64(&self[0..8])?,
+      target: if target_id == 0 { None } else { Some(target_id) },
+      spell_id: byte_reader::read_u32(&self[16..20])?,
+      hit_type: self[20]
+    })
   }
 }
