af3a0c1d18ec9c6b8c3de6b9d22935d9417a83c0
Tom Dymel <td@wps.de>
2020-04-03 10:05:28 +0200

    Mapping un aura

--- Backend/src/modules/live_data_processor/tools/payload_mapper/un_aura.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/un_aura.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, UnAura};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapUnAura {
   fn to_un_aura(&self) -> Result<UnAura, LiveDataProcessorFailure>;
@@ -6,6 +7,13 @@ pub trait MapUnAura {
 
 impl MapUnAura for [u8] {
   fn to_un_aura(&self) -> Result<UnAura, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(UnAura {
+      un_aura_caster: byte_reader::read_u64(&self[0..8])?,
+      target: byte_reader::read_u64(&self[8..16])?,
+      aura_caster: byte_reader::read_u64(&self[16..24])?,
+      un_aura_spell_id: byte_reader::read_u32(&self[24..28])?,
+      target_spell_id: byte_reader::read_u32(&self[28..32])?,
+      un_aura_amount: self[32]
+    })
   }
 }
