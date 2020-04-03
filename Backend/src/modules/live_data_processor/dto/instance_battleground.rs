c925134dfdeed7f88f956b0d22d6aeca1081566d
Tom Dymel <td@wps.de>
2020-04-03 10:56:08 +0200

    Mapping Instance Battleground

--- Backend/src/modules/live_data_processor/dto/instance_battleground.rs
+++ Backend/src/modules/live_data_processor/dto/instance_battleground.rs
@@ -1,7 +1,7 @@
 pub struct InstanceBattleground {
   pub map_id: u32,
   pub instance_id: u32,
-  pub winner: Option<u8>,
-  pub score_alliance: Option<u32>,
-  pub score_horde: Option<u32>
+  pub winner: u8,
+  pub score_alliance: u32,
+  pub score_horde: u32
 }
--- Backend/src/modules/live_data_processor/tools/payload_mapper/instance_battleground.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/instance_battleground.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, InstanceBattleground};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapInstanceBattleground {
   fn to_instance_battleground(&self) -> Result<InstanceBattleground, LiveDataProcessorFailure>;
@@ -6,6 +7,12 @@ pub trait MapInstanceBattleground {
 
 impl MapInstanceBattleground for [u8] {
   fn to_instance_battleground(&self) -> Result<InstanceBattleground, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(InstanceBattleground {
+      map_id: byte_reader::read_u32(&self[0..4])?,
+      instance_id: byte_reader::read_u32(&self[4..8])?,
+      winner: self[8],
+      score_alliance: byte_reader::read_u32(&self[9..13])?,
+      score_horde: byte_reader::read_u32(&self[13..17])?
+    })
   }
 }
