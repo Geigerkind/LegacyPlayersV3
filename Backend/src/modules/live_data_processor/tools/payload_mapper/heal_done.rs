da44d1f6b57226b6d446ccdae118d3560702cdb2
Tom Dymel <td@wps.de>
2020-04-03 09:50:11 +0200

    Mapping Heal done

--- Backend/src/modules/live_data_processor/tools/payload_mapper/heal_done.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/heal_done.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, HealDone};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapHealDone {
   fn to_heal_done(&self) -> Result<HealDone, LiveDataProcessorFailure>;
@@ -6,6 +7,13 @@ pub trait MapHealDone {
 
 impl MapHealDone for [u8] {
   fn to_heal_done(&self) -> Result<HealDone, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(HealDone {
+      caster: byte_reader::read_u64(&self[0..8])?,
+      target: byte_reader::read_u64(&self[8..16])?,
+      spell_id: byte_reader::read_u32(&self[16..20])?,
+      total_heal: byte_reader::read_u32(&self[20..24])?,
+      effective_heal: byte_reader::read_u32(&self[24..28])?,
+      absorb: byte_reader::read_u32(&self[28..32])?
+    })
   }
 }
