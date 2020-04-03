e81c64fb739f966d4612ef9161f78c94c78a8084
Tom Dymel <td@wps.de>
2020-04-03 11:01:53 +0200

    Mapping threat

--- Backend/src/modules/live_data_processor/tools/payload_mapper/threat.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/threat.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, Threat};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapThreat {
   fn to_threat(&self) -> Result<Threat, LiveDataProcessorFailure>;
@@ -6,6 +7,11 @@ pub trait MapThreat {
 
 impl MapThreat for [u8] {
   fn to_threat(&self) -> Result<Threat, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(Threat {
+      threater: byte_reader::read_u64(&self[0..8])?,
+      threatened: byte_reader::read_u64(&self[8..16])?,
+      spell_id: byte_reader::read_u32(&self[16..20])?,
+      amount: byte_reader::read_i32(&self[20..24])?
+    })
   }
 }
