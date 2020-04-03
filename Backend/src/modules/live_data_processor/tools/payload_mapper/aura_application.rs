0e6328877ac33cefbbf7595269e5df9a3fab370c
Tom Dymel <td@wps.de>
2020-04-03 09:56:53 +0200

    Mapping aura application

--- Backend/src/modules/live_data_processor/tools/payload_mapper/aura_application.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/aura_application.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, AuraApplication};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapAuraApplication {
   fn to_aura_application(&self) -> Result<AuraApplication, LiveDataProcessorFailure>;
@@ -6,6 +7,12 @@ pub trait MapAuraApplication {
 
 impl MapAuraApplication for [u8] {
   fn to_aura_application(&self) -> Result<AuraApplication, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(AuraApplication {
+      caster: byte_reader::read_u64(&self[0..8])?,
+      target: byte_reader::read_u64(&self[8..16])?,
+      spell_id: byte_reader::read_u32(&self[16..20])?,
+      stack_amount: self[20],
+      applied: if self[21] == 1 { true } else { false }
+    })
   }
 }
