6c50f0e3562360c7e43b2a0b8c5be007121d54f5
Tom Dymel <td@wps.de>
2020-04-03 10:11:56 +0200

    Mapping Position

--- Backend/src/modules/live_data_processor/tools/payload_mapper/position.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/position.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, Position};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapPosition {
   fn to_position(&self) -> Result<Position, LiveDataProcessorFailure>;
@@ -6,6 +7,15 @@ pub trait MapPosition {
 
 impl MapPosition for [u8] {
   fn to_position(&self) -> Result<Position, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(Position {
+      map_id: byte_reader::read_u32(&self[0..4])?,
+      instance_id: byte_reader::read_u32(&self[4..8])?,
+      map_difficulty: self[8],
+      unit: byte_reader::read_u64(&self[9..17])?,
+      x: byte_reader::read_i32(&self[17..21])?,
+      y: byte_reader::read_i32(&self[21..25])?,
+      z: byte_reader::read_i32(&self[25..29])?,
+      orientation: byte_reader::read_i32(&self[29..33])?
+    })
   }
 }
