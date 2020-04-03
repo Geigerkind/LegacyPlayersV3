f99e39e065211027a33daac1fd3780e5d25af99b
Tom Dymel <td@wps.de>
2020-04-03 10:38:03 +0200

    Mapping InstanceArena

--- Backend/src/modules/live_data_processor/tools/payload_mapper/instance_arena.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/instance_arena.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, InstanceArena};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapInstanceArena {
   fn to_instance_arena(&self) -> Result<InstanceArena, LiveDataProcessorFailure>;
@@ -6,6 +7,14 @@ pub trait MapInstanceArena {
 
 impl MapInstanceArena for [u8] {
   fn to_instance_arena(&self) -> Result<InstanceArena, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(InstanceArena {
+      map_id: byte_reader::read_u32(&self[0..4])?,
+      instance_id: byte_reader::read_u32(&self[4..8])?,
+      winner: self[8],
+      team_id1: byte_reader::read_u32(&self[9..13])?,
+      team_id2: byte_reader::read_u32(&self[13..17])?,
+      team_change1: byte_reader::read_i32(&self[17..21])?,
+      team_change2: byte_reader::read_i32(&self[21..24])?
+    })
   }
 }
