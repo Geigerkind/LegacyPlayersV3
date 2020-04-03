1e9179dde741cfe244682e772f1d6c5601348bf1
Tom Dymel <td@wps.de>
2020-04-03 12:38:26 +0200

    Aura application negative test

--- Backend/src/modules/live_data_processor/tools/payload_mapper/tests/aura_application.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/tests/aura_application.rs
@@ -1,7 +1,7 @@
 use crate::modules::live_data_processor::tools::payload_mapper::aura_application::MapAuraApplication;
 
 #[test]
-fn map_aura_application() {
+fn map_aura_application_positive() {
   // Arrange
   let payload = vec![
     1, 0, 0, 0, 0, 0, 0, 0, // Caster
@@ -22,4 +22,16 @@ fn map_aura_application() {
   assert_eq!(aura_application.spell_id, 23);
   assert_eq!(aura_application.stack_amount, 3);
   assert_eq!(aura_application.applied, true);
+}
+
+#[test]
+fn map_aura_application_negative() {
+  // Arrange
+  let payload = vec![1,2,3,4,5];
+
+  // Act
+  let aura_application = payload.to_aura_application();
+
+  // Assert
+  assert!(aura_application.is_err());
 }
