abaa935508656fb1a3e08f19327f2d9f9abecd1b
Tom Dymel <td@wps.de>
2020-04-03 10:24:52 +0200

    Mapping Power

--- Backend/src/modules/live_data_processor/tools/payload_mapper/power.rs
+++ Backend/src/modules/live_data_processor/tools/payload_mapper/power.rs
@@ -1,4 +1,5 @@
 use crate::modules::live_data_processor::dto::{LiveDataProcessorFailure, Power};
+use crate::modules::live_data_processor::tools::byte_reader;
 
 pub trait MapPower {
   fn to_power(&self) -> Result<Power, LiveDataProcessorFailure>;
@@ -6,6 +7,11 @@ pub trait MapPower {
 
 impl MapPower for [u8] {
   fn to_power(&self) -> Result<Power, LiveDataProcessorFailure> {
-    unimplemented!()
+    Ok(Power {
+      unit: byte_reader::read_u64(&self[0..8])?,
+      power_type: self[8],
+      max_power: byte_reader::read_u32(&self[9..13])?,
+      current_power: byte_reader::read_u32(&self[13..17])?
+    })
   }
 }
