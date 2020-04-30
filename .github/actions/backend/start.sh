834b67808184aaa1260ddd485de203c77ea6628a
Tom Dymel <td@wps.de>
2020-04-30 10:49:59 +0200

    Clone current branch

--- .github/actions/backend/start.sh
+++ .github/actions/backend/start.sh
@@ -1,7 +1,7 @@
 echo ${GITHUB_TOKEN}
-git clone https://Geigerkind:${GITHUB_TOKEN}@github.com/Geigerkind/LegacyPlayersV3
+git clone --single-branch --branch ${BRANCH_NAME} https://Geigerkind:${GITHUB_TOKEN}@github.com/Geigerkind/LegacyPlayersV3
 cd LegacyPlayersV3/Backend
 cp .env_ci .env
 cargo build
-cargo test
+cargo test --features "integration"
 cargo clippy
