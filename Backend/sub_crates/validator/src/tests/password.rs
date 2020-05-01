4d59d1f4ff3fce3e15a1361248f697cf94ec37ea
Tom Dymel <td@wps.de>
2020-05-01 21:47:08 +0200

    Bringt Validator test wieder zum laufen

--- Backend/sub_crates/validator/src/tests/password.rs
+++ Backend/sub_crates/validator/src/tests/password.rs
@@ -1,21 +1,26 @@
 #[cfg(test)]
 mod tests {
+  extern crate dotenv;
   use crate::tools::valid_password;
+  use self::dotenv::dotenv;
 
   #[test]
   fn password_too_short() {
+    dotenv().ok();
     let pass = "tooshort";
     assert!(valid_password(pass).is_err());
   }
 
   #[test]
   fn password_has_been_pwned() {
+    dotenv().ok();
     let pass = "Password123456";
     assert!(valid_password(pass).is_err());
   }
 
   #[test]
   fn password_is_secure_enough() {
+    dotenv().ok();
     let pass = "Password123456Password123456Password123456";
     assert!(valid_password(pass).is_ok());
   }
