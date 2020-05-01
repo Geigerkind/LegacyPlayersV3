e63872fd9d2645ce5ddeb5aecc4e8c696d4d99e9
Hendrik <dev.nipe.systems@gmail.com>
2020-05-01 12:13:15 +0200

    Add comments and improve readability

--- Backend/sub_crates/str_util/src/tests/strformat.rs
+++ Backend/sub_crates/str_util/src/tests/strformat.rs
@@ -3,11 +3,27 @@ use self::proptest::prelude::*;
 use crate::strformat;
 
 proptest! {
+  /// This test is supplied with three random inputs:
+  ///
+  /// 1. Size of the generated arguments, in interval `[0,32)`
+  /// 2. Permutations of strings for generated arguments, `size = 32`
+  /// 3. Strings to pick from, `size = 32`
+  ///
+  /// The idea is to generate a format string with random indices, e.g. `"{2} {4} {0} {0}"`. The function `strformat::fmt()` then inserts the corresponding values from `strings`. Consider a `strings` array of `["A", "B", "C", "D", "E", "F"]` we get a format string of `"C E A A"`.
   #[test]
   fn test_arguments(size in 0usize..33usize, permutations in prop::array::uniform32(0usize..32usize), strings in prop::array::uniform32("\\PC*")) {
-    let format = (0..size).map(|i| format!("{{{}}}", permutations[i])).collect::<Vec<_>>().join(" ");
-    let arguments = strings.iter().map(|item| &item[..]).collect::<Vec<_>>();
-    let expected = (0..size).map(|i| &strings[permutations[i]][..]).collect::<Vec<_>>().join(" ");
+    let format = (0..size)
+      .map(|i| format!("{{{}}}", permutations[i]))
+      .collect::<Vec<_>>()
+      .join(" ");
+    let arguments = strings
+      .iter()
+      .map(|item| &item[..])
+      .collect::<Vec<_>>();
+    let expected = (0..size)
+      .map(|i| &strings[permutations[i]][..])
+      .collect::<Vec<_>>()
+      .join(" ");
     assert_eq!(strformat::fmt(format, &arguments), expected);
   }
 }
