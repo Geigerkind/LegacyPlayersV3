943bc7501f5371dcfb591af6d9fba6a46894712f
Tom Dymel <td@wps.de>
2020-05-19 18:48:21 +0200

    Updates pipeline to for grcov

--- .github/actions/backend/start.sh
+++ .github/actions/backend/start.sh
@@ -5,7 +5,8 @@ cp .env_ci .env
 cargo install grcov
 
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+export RUSTDOCFLAGS="-Cpanic=abort"
 cargo build;
 cargo test --features "integration";
 ~/.cargo/bin/grcov ./target/debug/ -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" --ignore "*/tests/*" --ignore "*/dto/*" --ignore "*/domain_value/*" -o lcov.info;
