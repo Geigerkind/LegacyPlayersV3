eeae91510634086b9bbcf5116600ad94f64cf7ce
Tom Dymel <td@wps.de>
2020-06-17 13:58:01 +0200

    Adding caching to the pipeline

--- Tools/coverage_grcov.sh
+++ Tools/coverage_grcov.sh
@@ -4,7 +4,7 @@ export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Co
 export RUSTDOCFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
 cargo clean;
 cargo update;
-cargo test --workspace;
+cargo test --workspace --all-features --no-fail-fast;
 ~/.cargo/bin/grcov ./target/debug/ -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" --ignore "*/tests/*" --ignore "*/dto/*" --ignore "*/domain_value/*" -o lcov.info;
 genhtml -o ./target/debug/coverage/ --branch-coverage --show-details --highlight --ignore-errors source --legend ./lcov.info
 rm ./lcov.info
