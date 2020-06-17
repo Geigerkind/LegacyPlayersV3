584f23a55159e0dab1804a3d6720b6df48d280f3
Tom Dymel <td@wps.de>
2020-06-17 16:53:26 +0200

    Different execution order

--- Tools/coverage_grcov.sh
+++ Tools/coverage_grcov.sh
@@ -1,10 +1,11 @@
 cargo install grcov
+cargo clean;
+cargo update;
+
 export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
 export RUSTDOCFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
-cargo clean;
-cargo update;
-cargo test --workspace --all-features --no-fail-fast;
+cargo test --workspace --all-features --no-fail-fast -- --test-threads=32;
 ~/.cargo/bin/grcov ./target/debug/ -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" --ignore "*/tests/*" --ignore "*/dto/*" --ignore "*/domain_value/*" -o lcov.info;
 genhtml -o ./target/debug/coverage/ --branch-coverage --show-details --highlight --ignore-errors source --legend ./lcov.info
 rm ./lcov.info
