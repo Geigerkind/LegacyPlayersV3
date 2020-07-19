f968b9a1e81ff26bb9142a0cdb3ea1e44345c739
Tom Dymel <td@wps.de>
2020-07-19 16:48:59 +0200

    Drafting out viewer graph component

--- Webclient/src/app/module/viewer/module/raid_graph/module.ts
+++ Webclient/src/app/module/viewer/module/raid_graph/module.ts
@@ -2,12 +2,16 @@ import {NgModule} from "@angular/core";
 import {TranslateModule} from "@ngx-translate/core";
 import {RaidGraphComponent} from "./component/raid_graph/raid_graph";
 import {CommonModule} from "@angular/common";
+import {MultiSelectModule} from "../../../../template/input/multi_select/module";
+import {SelectInputModule} from "../../../../template/input/select_input/module";
 
 @NgModule({
     declarations: [RaidGraphComponent],
     imports: [
         CommonModule,
-        TranslateModule
+        TranslateModule,
+        MultiSelectModule,
+        SelectInputModule
     ],
     exports: [RaidGraphComponent]
 })
