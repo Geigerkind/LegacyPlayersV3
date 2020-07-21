3a731a77bce189807e711fbfd354258251dda181
Tom Dymel <td@wps.de>
2020-07-21 18:35:49 +0200

    Style for Raid composition

--- Webclient/src/app/module/viewer/module/raid_composition/component/raid_composition/raid_composition.ts
+++ Webclient/src/app/module/viewer/module/raid_composition/component/raid_composition/raid_composition.ts
@@ -5,4 +5,12 @@ import {Component} from "@angular/core";
     templateUrl: "./raid_composition.html",
     styleUrls: ["./raid_composition.scss"]
 })
-export class RaidCompositionComponent {}
+export class RaidCompositionComponent {
+
+    temp_classes = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+    show_tanks: boolean = false;
+    show_healers: boolean = false;
+    show_dps: boolean = false;
+
+}
--- Webclient/src/app/module/viewer/module/raid_composition/module.ts
+++ Webclient/src/app/module/viewer/module/raid_composition/module.ts
@@ -2,12 +2,14 @@ import {NgModule} from "@angular/core";
 import {TranslateModule} from "@ngx-translate/core";
 import {RaidCompositionComponent} from "./component/raid_composition/raid_composition";
 import {CommonModule} from "@angular/common";
+import {CaretButtonModule} from "../../../../template/button/caret_button/module";
 
 @NgModule({
     declarations: [RaidCompositionComponent],
     imports: [
         CommonModule,
-        TranslateModule
+        TranslateModule,
+        CaretButtonModule
     ],
     exports: [RaidCompositionComponent]
 })
