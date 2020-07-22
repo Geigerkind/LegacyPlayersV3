697787bdbb217cd534ed60be9da0a6b47078b422
Tom Dymel <td@wps.de>
2020-07-22 19:58:06 +0200

    Raid detail table styling

--- Webclient/src/app/module/viewer/module/raid_detail_table/module.ts
+++ Webclient/src/app/module/viewer/module/raid_detail_table/module.ts
@@ -2,12 +2,14 @@ import {NgModule} from "@angular/core";
 import {TranslateModule} from "@ngx-translate/core";
 import {RaidDetailTableComponent} from "./component/raid_detail_table/raid_detail_table";
 import {CommonModule} from "@angular/common";
+import {SelectInputModule} from "../../../../template/input/select_input/module";
 
 @NgModule({
     declarations: [RaidDetailTableComponent],
     imports: [
         CommonModule,
-        TranslateModule
+        TranslateModule,
+        SelectInputModule
     ],
     exports: [RaidDetailTableComponent]
 })
