05c884dbf1c6131fedfa38ef69f0d7a53b791504
Tom Dymel <td@wps.de>
2020-08-07 09:37:21 +0200

    Extracting either offline players once or online players that are in instances every configured interval

--- Exporter/Backend/src/modules/armory_exporter/tools/character.rs
+++ Exporter/Backend/src/modules/armory_exporter/tools/character.rs
@@ -12,7 +12,9 @@ impl RetrieveRecentOfflineCharacters for ArmoryExporter {
         let last_fetch_time = self.last_fetch_time;
         db_characters
             .select_wparams(
-                "SELECT guid, name, race, class, gender, level, chosenTitle, playerBytes, playerBytes2 FROM characters WHERE online=1 OR (online=0 AND logout_time > :last_fetch_time)",
+                "SELECT guid, name, race, class, gender, level, chosenTitle, playerBytes, playerBytes2 FROM characters \
+                    WHERE (online=0 AND logout_time > :last_fetch_time) OR (online=1 AND \
+                    `map` IN (249,309,409,469,509,531,532,533,534,544,548,550,564,565,568,580,603,615,616,624,631,649,724,559,562,572,617,618,30,489,529,566,607,628))",
                 |mut row| CharacterTable {
                     character_id: row.take(0).unwrap(),
                     name: row.take(1).unwrap(),
