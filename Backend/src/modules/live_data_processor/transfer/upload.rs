f2e5aaed7bf42bd745f9ebddb1c58e7ede854fee
Tom Dymel <td@wps.de>
2020-08-30 21:11:12 +0200

    Adjustin test upload path

--- Backend/src/modules/live_data_processor/transfer/upload.rs
+++ Backend/src/modules/live_data_processor/transfer/upload.rs
@@ -11,7 +11,7 @@ use rocket::State;
 #[openapi]
 #[get("/upload")]
 pub fn upload_log(mut db_main: MainDb, me: State<LiveDataProcessor>, data: State<Data>, armory: State<Armory>) -> Result<(), LiveDataProcessorFailure> {
-    let messages = WoWCBTLParser::new(&data, 6, 0, u64::MAX, 0).parse(&mut *db_main, &data, &armory, "/home/shino/Downloads/+0100_Hayleigh_000_100_Feierabend_2020_08_27_naxx_25.txt");
+    let messages = WoWCBTLParser::new(&data, 6, 0, u64::MAX, 0).parse(&mut *db_main, &data, &armory, "/home/rpll/+0100_Hayleigh_000_100_Feierabend_2020_08_27_naxx_25.txt");
 
     me.process_messages(&mut *db_main, 6, &armory, &data, messages)
 }
