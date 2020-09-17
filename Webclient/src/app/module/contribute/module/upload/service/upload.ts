4dbe171db7c34268b3d850090d0419fc19c8ebf6
Tom Dymel <td@wps.de>
2020-09-17 17:40:57 +0200

    Adding notifications

--- Webclient/src/app/module/contribute/module/upload/service/upload.ts
+++ Webclient/src/app/module/contribute/module/upload/service/upload.ts
@@ -1,5 +1,7 @@
 import {Injectable} from "@angular/core";
 import {APIService} from "../../../../../service/api";
+import {NotificationService} from "../../../../../service/notification";
+import {Severity} from "../../../../../domain_value/severity";
 
 @Injectable({
     providedIn: "root",
@@ -8,11 +10,17 @@ export class UploadService {
     private static readonly URL_UPLOAD: string = "/live_data_processor/upload";
 
     constructor(
-        private apiService: APIService
+        private apiService: APIService,
+        private notification_service: NotificationService
     ) {
     }
 
     upload_file(form_data: FormData): void {
-        this.apiService.post_form_data(UploadService.URL_UPLOAD, form_data, () => console.log("SUCCESS"), () => console.log("FAILURE!"));
+        this.notification_service.propagate(Severity.Info, "Uploading...");
+        this.apiService.post_form_data(UploadService.URL_UPLOAD, form_data, () => {
+            this.notification_service.propagate(Severity.Success, "Your log has been uploaded!");
+        }, () => {
+            this.notification_service.propagate(Severity.Error, "Your log failed to upload!");
+        });
     }
 }
