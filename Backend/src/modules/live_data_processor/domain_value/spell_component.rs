66651fd336a9019aecba3604d58fa9f583b07b2b
Tom Dymel <td@wps.de>
2020-11-09 20:37:32 +0100

    Also considering it for rankings

--- Backend/src/modules/live_data_processor/domain_value/spell_component.rs
+++ Backend/src/modules/live_data_processor/domain_value/spell_component.rs
@@ -8,5 +8,10 @@ pub struct SpellComponent {
 }
 
 pub fn get_spell_components_total(components: &Vec<SpellComponent>) -> u32 {
-    components.iter().fold(0, |acc, comp| acc + comp.amount)
+    components.iter().fold(0, |acc, comp| acc + comp.amount + comp.mitigation.iter().find_map(|mitigation| {
+        if let Mitigation::Absorb(amount) = mitigation {
+            return Some(*amount);
+        }
+        None
+    }).unwrap_or(0))
 }
