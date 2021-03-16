22f0625e11d13b58380f9cf493da1b0b3358f83f
Tom Dymel <td@wps.de>
2021-03-16 14:09:43 +0100

    A raid is now considered from a guild if 55% of all KNOWN participants is from the same guild

--- Backend/src/modules/instance/tools/instance_guild.rs
+++ Backend/src/modules/instance/tools/instance_guild.rs
@@ -1,8 +1,9 @@
+use grouping_by::GroupingBy;
+
+use crate::modules::armory::Armory;
 use crate::modules::armory::material::Guild;
 use crate::modules::armory::tools::{GetCharacter, GetGuild};
-use crate::modules::armory::Armory;
 use crate::util::database::Select;
-use grouping_by::GroupingBy;
 
 pub trait FindInstanceGuild {
     fn find_instance_guild(&self, db_main: &mut impl Select, armory: &Armory, timestamp: u64) -> Option<Guild>;
@@ -11,18 +12,14 @@ pub trait FindInstanceGuild {
 // Where u32 is a character_id
 impl FindInstanceGuild for Vec<u32> {
     fn find_instance_guild(&self, db_main: &mut impl Select, armory: &Armory, timestamp: u64) -> Option<Guild> {
-        let same_guild_fraction = 0.75;
-        for (guild_id, group) in &self
-            .iter()
-            .map(|character_id| {
-                if let Some(character_history) = armory.get_character_moment(db_main, *character_id, timestamp) {
-                    return character_history.character_guild.map(|inner| inner.guild_id);
-                }
-                None
-            })
-            .grouping_by(|character_guild_id| *character_guild_id)
-        {
-            if (group.len() as f64) / (self.len() as f64) >= same_guild_fraction {
+        let valid_participants = &self.iter()
+            .filter_map(|character_id| armory.get_character_moment(db_main, *character_id, timestamp))
+            .filter(|history| history.character_info.hero_class_id != 12)
+            .map(|history| history.character_guild.map(|inner| inner.guild_id))
+            .collect::<Vec<Option<u32>>>();
+        let same_guild_fraction = 0.55;
+        for (guild_id, group) in &valid_participants.iter().grouping_by(|character_guild_id| *character_guild_id) {
+            if (group.len() as f64) / (valid_participants.len() as f64) >= same_guild_fraction {
                 return guild_id.and_then(|guild_id| armory.get_guild(guild_id));
             }
         }
