9fa2df3f3977be2d9c0c67d8ce9c58b010b16e4b
Tom Dymel <td@wps.de>
2021-05-19 21:05:13 +0200

    Implement db rotating

--- Deploy/backup_db.sh
+++ Deploy/backup_db.sh
@@ -4,18 +4,31 @@ if [ "${MYSQL_UP}" -le 1 ]; then
 fi
 
 DB_PASSWORD=$(cat /root/Keys/db_password)
-TIMESTAMP=$(date +%s)
+OFFSET=$((cat /root/DB_BACKUPS/current_offset))
+LOG_1_PREV=${OFFSET}
+LOG_1_NEXT=$((OFFSET + 1))
+LOG_2_PREV=$((OFFSET + 6))
+LOG_2_NEXT=$((OFFSET + 7))
+LOG_3_PREV=$((OFFSET + 13))
+LOG_3_NEXT=$((OFFSET + 14))
 
-echo "Removing dumps older than 14 days..."
-find /root/DB_BACKUPS/*.zip -mtime +14 -exec rm {} \;
+if (( OFFSET == 6 )); then
+  echo "1" > /root/DB_BACKUPS/current_offset
+else
+  echo "$((OFFSET + 1))" > /root/DB_BACKUPS/current_offset
+fi
+
+mv /root/DB_BACKUPS/Backup_${LOG_3_PREV}.zip /root/DB_BACKUPS/Backup_${LOG_3_NEXT}.zip
+mv /root/DB_BACKUPS/Backup_${LOG_2_PREV}.zip /root/DB_BACKUPS/Backup_${LOG_2_NEXT}.zip
+mv /root/DB_BACKUPS/Backup_${LOG_1_PREV}.zip /root/DB_BACKUPS/Backup_${LOG_1_NEXT}.zip
 
 echo "Dumping database..."
-mariabackup --backup --target-dir /root/DB_BACKUPS/${TIMESTAMP} --user=root --password=${DB_PASSWORD}
+mariabackup --backup --target-dir /root/DB_BACKUPS/Backup_1 --user=root --password=${DB_PASSWORD}
 
 echo "Zipping the dump..."
-zip -r /root/DB_BACKUPS/${TIMESTAMP}.zip /root/DB_BACKUPS/${TIMESTAMP}
+zip -r /root/DB_BACKUPS/Backup_1.zip /root/DB_BACKUPS/Backup_1
 
 echo "Removing unzipped files..."
-rm -rf /root/DB_BACKUPS/${TIMESTAMP}
+rm -rf /root/DB_BACKUPS/Backup_1
 
 echo "DONE!"
