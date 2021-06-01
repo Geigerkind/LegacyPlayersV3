62508c92bb59723bc6da6fed73281fc934418ad3
Tom Dymel <td@wps.de>
2021-06-01 13:02:04 +0200

    Fix guild speed kill error

--- Webclient/src/app/module/armory/module/guild_viewer/module/speed_kills/component/speed_kills/speed_kills.ts
+++ Webclient/src/app/module/armory/module/guild_viewer/module/speed_kills/component/speed_kills/speed_kills.ts
@@ -80,10 +80,8 @@ export class SpeedKillsComponent implements OnInit, OnDestroy {
         this.subscriptions.add(this.speedKillService.all_speed_kills.subscribe(speed_kills => {
             const data = group_by(speed_kills.filter(speed_kill => speed_kill.guild_id === this.guild_id), (speed_kill) => speed_kill.encounter_id);
             const result = [];
-            for (const group in data) {
-                console.log(group, data[group]);
+            for (const group in data)
                 result.push(min_by(data[group], (entry) => entry.duration));
-            }
             this.update(result);
         }));
     }
@@ -99,7 +97,6 @@ export class SpeedKillsComponent implements OnInit, OnDestroy {
     update(speed_kills: Array<SpeedKill>): void {
         this.body_columns = [];
         for (const speed_kill of speed_kills) {
-            console.log("SK:", speed_kill);
             this.body_columns.push({
                 color: "", columns: [
                     {
--- Webclient/src/app/stdlib/max_by.ts
+++ Webclient/src/app/stdlib/max_by.ts
@@ -2,7 +2,7 @@ export function max_by(input: Array<any>, lambda): any {
     let prev_result = input[0];
     for (let i=1; i<input.length; ++i) {
         if (lambda(prev_result) < lambda(input[i])) {
-            prev_result = input;
+            prev_result = input[i];
         }
     }
     return prev_result;
