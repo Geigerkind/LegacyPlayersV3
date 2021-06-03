b2ed6b7ce457caa640590c44ecbdf21549d1c5fa
Tom Dymel <td@wps.de>
2021-06-03 16:37:31 +0200

    Filling event type multi select, implement 'everything' option

--- Webclient/src/app/module/viewer/module/raid_configuration_menu/module/raid_browser/module/popup_add/component/popup_add/popup_add.ts
+++ Webclient/src/app/module/viewer/module/raid_configuration_menu/module/raid_browser/module/popup_add/component/popup_add/popup_add.ts
@@ -10,12 +10,33 @@ import {Severity} from "../../../../../../../../../../domain_value/severity";
     styleUrls: ["./popup_add.scss"]
 })
 export class PopupAddComponent implements OnChanges {
+    private static EVENT_TYPES: Array<[number, string]> = [
+        [0, "Spell casts"],
+        [1, "Deaths"],
+        [2, "Combat states"],
+        [3, "Loot"],
+        [4, "Positions"],
+        [5, "Power gained"],
+        [6, "Aura applications"],
+        [7, "Interrupts"],
+        [8, "Spell steals"],
+        [9, "Dispels"],
+        [10, "Threat wipes"],
+        [11, "Summons"],
+        [12, "Melee damage"],
+        [13, "Spell damage"],
+        [14, "Heal"],
+        [15, "Threat"]
+    ];
 
     @Input() presets: Array<Preset>;
     @Output() close_widget: EventEmitter<void> = new EventEmitter();
     @Output() add_preset: EventEmitter<Preset> = new EventEmitter();
 
-    selected_option: string = "";
+    load_presets: Array<SelectOption> = [];
+    event_types: Array<any> = [];
+
+    selected_preset: string = "";
     preset_name: string = "";
     selected_event_types: Array<any> = [];
     selected_sources: Array<any> = [];
@@ -29,18 +50,27 @@ export class PopupAddComponent implements OnChanges {
     }
 
     ngOnChanges(): void {
-        if (this.presets.length > 0 && this.selected_option === "")
-            this.selected_option = this.presets[0].name;
+        if (this.presets.length > 0 && this.selected_preset === "")
+            this.selected_preset = this.presets[0].name;
+
+        this.load_presets = this.select_presets;
+        this.event_types = this.select_event_types;
     }
 
-    get select_options(): Array<SelectOption> {
+    get select_presets(): Array<SelectOption> {
         return this.presets.map(preset => {
             return {value: preset.name, label_key: preset.name};
         });
     }
 
+    get select_event_types(): Array<any> {
+        return [{id: -1, label: "Everything"}].concat(PopupAddComponent.EVENT_TYPES.map(evt_type => {
+            return {id: evt_type[0], label: evt_type[1]};
+        }));
+    }
+
     load_preset(): void {
-        this.preset_name = this.selected_option;
+        this.preset_name = this.selected_preset;
     }
 
     save_preset(): void {
@@ -57,5 +87,17 @@ export class PopupAddComponent implements OnChanges {
             abilities: this.selected_abilities.map(item => item.id),
             hit_types: this.selected_hit_types.map(item => item.id),
         });
+    }
+
+    selected_event_types_changed(selected_items: Array<any>): void {
+        const old_contains_everything: boolean = !!this.selected_event_types.find(item => item.id === -1);
+        const new_contains_everything: boolean = !!selected_items.find(item => item.id === -1);
+        if (!old_contains_everything && new_contains_everything) {
+            this.selected_event_types = selected_items.filter(item => item.id === -1);
+        } else if (old_contains_everything && selected_items.length > 1) {
+            this.selected_event_types = selected_items.filter(item => item.id !== -1);
+        } else {
+            this.selected_event_types = selected_items;
+        }
     }
 }
