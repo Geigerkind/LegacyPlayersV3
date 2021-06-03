bf16f1bdeded62d707c9875d7d555269ca3264f0
Tom Dymel <td@wps.de>
2021-06-03 16:48:18 +0200

    Filling hit type multi select

--- Webclient/src/app/module/viewer/module/raid_configuration_menu/module/raid_browser/module/popup_add/component/popup_add/popup_add.ts
+++ Webclient/src/app/module/viewer/module/raid_configuration_menu/module/raid_browser/module/popup_add/component/popup_add/popup_add.ts
@@ -28,6 +28,29 @@ export class PopupAddComponent implements OnChanges {
         [14, "Heal"],
         [15, "Threat"]
     ];
+    private static HIT_TYPES: Array<[number, string]> = [
+        [1, "OffHand"],
+        [2, "Hit"],
+        [4, "Crit"],
+        [8, "Partial resist"],
+        [16, "Full resist"],
+        [32, "Miss"],
+        [64, "Partial absorb"],
+        [128, "Full absorb"],
+        [256, "Glancing"],
+        [512, "Crushing"],
+        [1024, "Evade"],
+        [2048, "Dodge"],
+        [4096, "Parry"],
+        [8192, "Immune"],
+        [16384, "Environment"],
+        [32768, "Deflect"],
+        [65536, "Interrupt"],
+        [131072, "Partial block"],
+        [262144, "Full block"],
+        [524288, "Split"],
+        [1048576, "Reflect"]
+    ];
 
     @Input() presets: Array<Preset>;
     @Output() close_widget: EventEmitter<void> = new EventEmitter();
@@ -35,6 +58,7 @@ export class PopupAddComponent implements OnChanges {
 
     load_presets: Array<SelectOption> = [];
     event_types: Array<any> = [];
+    hit_types: Array<any> = [];
 
     selected_preset: string = "";
     preset_name: string = "";
@@ -55,6 +79,7 @@ export class PopupAddComponent implements OnChanges {
 
         this.load_presets = this.select_presets;
         this.event_types = this.select_event_types;
+        this.hit_types = this.select_hit_types;
     }
 
     get select_presets(): Array<SelectOption> {
@@ -65,6 +90,12 @@ export class PopupAddComponent implements OnChanges {
 
     get select_event_types(): Array<any> {
         return [{id: -1, label: "Everything"}].concat(PopupAddComponent.EVENT_TYPES.map(evt_type => {
+            return {id: evt_type[0], label: evt_type[1]};
+        }));
+    }
+
+    get select_hit_types(): Array<any> {
+        return [{id: -1, label: "Everything"}].concat(PopupAddComponent.HIT_TYPES.map(evt_type => {
             return {id: evt_type[0], label: evt_type[1]};
         }));
     }
@@ -98,6 +129,18 @@ export class PopupAddComponent implements OnChanges {
             this.selected_event_types = selected_items.filter(item => item.id !== -1);
         } else {
             this.selected_event_types = selected_items;
+        }
+    }
+
+    selected_hit_types_changed(selected_items: Array<any>): void {
+        const old_contains_everything: boolean = !!this.selected_hit_types.find(item => item.id === -1);
+        const new_contains_everything: boolean = !!selected_items.find(item => item.id === -1);
+        if (!old_contains_everything && new_contains_everything) {
+            this.selected_hit_types = selected_items.filter(item => item.id === -1);
+        } else if (old_contains_everything && selected_items.length > 1) {
+            this.selected_hit_types = selected_items.filter(item => item.id !== -1);
+        } else {
+            this.selected_hit_types = selected_items;
         }
     }
 }
