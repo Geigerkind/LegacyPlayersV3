038a491a27d475c9f76fadf7dc03c201de57b4d7
Tom Dymel <td@wps.de>
2021-06-04 19:41:15 +0200

    Reducing ranking season payload to minimal required

--- Backend/src/modules/instance/tools/ranking.rs
+++ Backend/src/modules/instance/tools/ranking.rs
@@ -8,33 +8,43 @@ use crate::modules::instance::dto::{RankingCharacterMeta, RankingResult};
 pub fn create_ranking_export(instance_metas: &HashMap<u32, InstanceMeta>, rankings: &HashMap<u32, HashMap<u32, Vec<RankingResult>>>, armory: &Armory, season: Option<u8>) -> Vec<(u32, Vec<(u32, RankingCharacterMeta, Vec<RankingResult>)>)> {
     rankings
         .iter()
-        .map(|(npc_id, char_rankings)| {
-            (
+        .filter_map(|(npc_id, char_rankings)| {
+            let res_char_rankings: Vec<(u32, RankingCharacterMeta, Vec<RankingResult>)> = char_rankings
+                .iter()
+                .filter_map(|(character_id, rankings)| {
+                    let res_rankings: Vec<RankingResult> = rankings.iter().filter_map(|rr| {
+                        let instance_meta = instance_metas.get(&rr.instance_meta_id)?;
+                        if instance_meta.privacy_type == PrivacyType::Public && (season.is_none() || season.contains(&rr.season_index)) {
+                            return Some(rr);
+                        }
+                        None
+                    }).cloned().collect();
+                    if res_rankings.len() == 0 {
+                        return None;
+                    }
+
+                    Some((
+                        *character_id,
+                        armory
+                            .get_character(*character_id)
+                            .map(|character| RankingCharacterMeta {
+                                server_id: character.server_id,
+                                hero_class_id: character.last_update.as_ref().map(|last_update| last_update.character_info.hero_class_id).unwrap_or_else(|| 0),
+                                name: character.last_update.as_ref().map(|last_update| last_update.character_name.clone()).unwrap_or_else(|| String::from("Unknown")),
+                            })
+                            .unwrap(),
+                        res_rankings
+                    ))
+                })
+                .collect();
+            if res_char_rankings.len() == 0 {
+                return None;
+            }
+
+            Some((
                 *npc_id,
-                char_rankings
-                    .iter()
-                    .map(|(character_id, rankings)| {
-                        (
-                            *character_id,
-                            armory
-                                .get_character(*character_id)
-                                .map(|character| RankingCharacterMeta {
-                                    server_id: character.server_id,
-                                    hero_class_id: character.last_update.as_ref().map(|last_update| last_update.character_info.hero_class_id).unwrap_or_else(|| 0),
-                                    name: character.last_update.as_ref().map(|last_update| last_update.character_name.clone()).unwrap_or_else(|| String::from("Unknown")),
-                                })
-                                .unwrap(),
-                            rankings.iter().filter_map(|rr| {
-                                let instance_meta = instance_metas.get(&rr.instance_meta_id)?;
-                                if instance_meta.privacy_type == PrivacyType::Public && (season.is_none() || season.contains(&rr.season_index)) {
-                                    return Some(rr);
-                                }
-                                None
-                            }).cloned().collect(),
-                        )
-                    })
-                    .collect(),
-            )
+                res_char_rankings
+            ))
         })
         .collect()
 }
