c5aa0c96fdbc8e3bb22f3b947add56cd4371e1fe
Tom Dymel <td@wps.de>
2021-06-05 10:38:08 +0200

    Edit paste form

--- Webclient/src/app/module/tools/module/addon_pastebin/module/edit/component/edit/edit.ts
+++ Webclient/src/app/module/tools/module/addon_pastebin/module/edit/component/edit/edit.ts
@@ -1,8 +1,34 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {DataService} from "../../../../../../../../service/data";
+import {SelectOption} from "../../../../../../../../template/input/select_input/domain_value/select_option";
+import {TAGS} from "../../../../../data/tags";
 
 @Component({
     selector: "Edit",
     templateUrl: "./edit.html",
     styleUrls: ["./edit.scss"]
 })
-export class EditComponent {}
+export class EditComponent implements OnInit {
+
+    expansions: Array<SelectOption> = [];
+    selected_expansion: number = 0;
+
+    tags: Array<any> = [];
+    selected_tags: Array<any> = [];
+
+    constructor(
+        private dataService: DataService
+    ) {
+        this.expansions = this.dataService.expansions;
+        this.tags = TAGS.map((tag, index) => {
+            return {id: index, label: tag};
+        });
+    }
+
+    ngOnInit(): void {
+    }
+
+    save_paste(): void {
+
+    }
+}
--- Webclient/src/app/module/tools/module/addon_pastebin/module/edit/module.ts
+++ Webclient/src/app/module/tools/module/addon_pastebin/module/edit/module.ts
@@ -3,13 +3,19 @@ import {TranslateModule} from "@ngx-translate/core";
 import {EditComponent} from "./component/edit/edit";
 import {CommonModule} from "@angular/common";
 import {EditRouting} from "./routing";
+import {GeneralInputModule} from "../../../../../../template/input/general_input/module";
+import {SelectInputModule} from "../../../../../../template/input/select_input/module";
+import {MultiSelectModule} from "../../../../../../template/input/multi_select/module";
 
 @NgModule({
     declarations: [EditComponent],
     imports: [
         CommonModule,
         TranslateModule,
-        EditRouting
+        EditRouting,
+        GeneralInputModule,
+        SelectInputModule,
+        MultiSelectModule
     ],
     exports: [EditComponent]
 })
